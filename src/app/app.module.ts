1a299b6f811e3b1ef3830b6bce1bbdc27b493983
victor rivera <victor.rivera@sofka.com.co>
2018-10-23 10:47:19 -0500

    feat (ng zorro) Add ng zorro dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { HttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import es from '@angular/common/locales/es';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { es_ES, NgZorroAntdModule, NZ_I18N } from 'ng-zorro-antd';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(es);
 
 export function createTranslationLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -22,9 +29,13 @@ export function createTranslationLoader(http: HttpClient) {
         useFactory: createTranslationLoader,
         deps: [HttpClient]
       }
-    })
+    }),
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: es_ES }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
